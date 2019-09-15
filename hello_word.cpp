43b829fe3ef72b01bf5c67c01189cbcd39784a1e
Ubuntu <ubuntu@ip-172-31-30-137.ec2.internal>
2019-09-15 16:23:26 +0000

    corrections made to hello_word.cpp

---
+++ hello_word.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Trinity Armstrong All rights reserved.
+//
+// Created by: Trinity Armstrong
+// Created on: September 2019
+// This is the "Hello, World!" program, but with nice style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
